0ee49d8195c1c07d1fcae8154f19b3006cb2ac00
João Miguel <joao.miguel@gruposbf.com.br>
2016-07-05 16:50:35 -0300

    Better error handling

--- test/index.js
+++ test/index.js
@@ -7,10 +7,15 @@ console.log("Starting action testing.");
 try {
     var action = new baseActionModule.BaseAction(testAction);
 
-    action.on("success", function () {
+    action.on("actionSuccess", function () {
         console.assert(true, "Action tested successfuly");
         console.log("Action tested successfuly");
         process.exit(0);
+    });
+
+    action.on("actionError", function () {
+      console.assert(false, "Erro testing action: %s", error);
+      process.abort();
     });
 
     action.execute();
