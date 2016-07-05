230b38d093f922ae3d968ba0735c5edb8b40f480
João Miguel <joao.miguel@gruposbf.com.br>
2016-07-05 10:41:32 -0300

    First code

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+const util = require("util");
+const debugLog = util.debuglog('gitci');
+const eventEmitter = require('events').EventEmitter;
+
+/**
+ * This is the base action that should be implemented by all the actions.
+ * @param  {Array} action 
+ */
+var BaseAction = function (action) {
+    eventEmitter.call(this);
+
+    this.on('newListener', function (listener) {
+        debuglog('Action event listener: %s', listener);
+        if (this.availableListeners.indexOf(listener) < 0)
+            throw new RangeError(util.format("There is no listener to the event \"%s\"", listener));
+    });
+
+    this.validade();
+}
+util.inherits(BaseAction, eventEmitter);
+
+/**
+ * The action object that will be executed.
+ */
+BaseAction.prototype.action = null;
+
+/**
+ * The available event listeners for this action.
+ */
+BaseAction.prototype.availableListeners = ["error"];
+
+/**
+ * The prototype of the execute function
+ */
+BaseAction.prototype.execute = function () {
+    return false;
+};
+
+BaseAction.prototype.validade = function () {
+    return false;
+};
+
+exports = module.exports = {
+    BaseAction: BaseAction
+}
